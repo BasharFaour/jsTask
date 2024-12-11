044e1d4d084ac6f6f93567b3b8b2f46d3d549587
BasharFaour <116457886+BasharFaour@users.noreply.github.com>
2024-12-11 22:53:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const username = prompt("Enter your username:");
 let attempts = 5;
 let password;
+let isAuthenticated = false;
 
 while (attempts > 0) {
     password = prompt("Enter your password:");
 
     if (password === "123") {
         alert(`Welcome, ${username}!`);
-        
+        isAuthenticated = true;
+        break; 
     } else {
         attempts--;
         if (attempts > 0) {
             alert(`Incorrect password. You have ${attempts} attempt(s) left.`);
-            }
+        }
     }
-    }
- alert("Please try again later.");
+}
 
+if (!isAuthenticated) {
+    alert("Please try again later.");
+}
